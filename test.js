262a462f1c5c85f0750435b3937c73cb987f0a75
James Monger <jameskmonger@hotmail.co.uk>
2016-04-19 00:32:50 +0100

    Test does not contain goal

--- test.js
+++ test.js
@@ -28,3 +28,15 @@ test('closest is goal when array contains goal', (t, array, goal) => {
     [ [ 6, 7, 8, 10], 10 ],
     [ [ 200, 12, 22, 22.6], 200 ]
 ]);
+
+test('closest is closest when array does not contain goal', (t, array, goal, expected) => {
+
+    let output = closest(array, goal);
+
+    t.assert.equal(expected, output);
+
+}, [
+    [ [ 20, 6, 10], 5, 6 ],
+    [ [ 6, 7, 8, 10], 200, 10 ],
+    [ [ 200, 12, 22, 22.6], 0, 12 ]
+]);
