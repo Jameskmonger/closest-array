a30cd5f07534259a24930f8bd3313caba77e043c
James Monger <jameskmonger@hotmail.co.uk>
2016-04-19 00:31:06 +0100

    Test only value

--- test.js
+++ test.js
@@ -2,3 +2,16 @@
 
 const test = require('modunit');
 const closest = require('./index');
+
+test('closest is goal when array contains goal only', (t, goal) => {
+
+    let array = [ goal ];
+    let output = closest(goal);
+
+    t.assert.equal(goal, output);
+
+}, [
+    [ 5 ],
+    [ 10 ],
+    [ 200 ]
+]);
